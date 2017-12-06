28225f72255bc643d483e46c759e33a73b18bd4c
michellele994 <michellele@email.arizona.edu>
2017-12-06 02:34:38 -0700

    Completed, fixed error with HTTP/HTTPS

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -29,7 +29,7 @@ $(document).ready(function(){
 
 		var currTopic = $(this).attr("data-name");
 		var fixedTopic = currTopic.split(" ").join("+");
-		var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + currTopic +"&api_key=dc6zaTOxFJmzC&limit=" +numberOfGif;
+		var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + currTopic +"&api_key=dc6zaTOxFJmzC&limit=" +numberOfGif;
 
 		$.ajax({
 			url: queryURL,
